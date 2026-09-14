c156dbfea5864c531249df4c9e281a9ffcc24886
Par Bahrae <githubproproject@proton.me>
2026-09-14 00:14:53 -0400

    Use pairwise for frontier monotonicity test

--- tests/test_advanced_portfolio.py
+++ tests/test_advanced_portfolio.py
@@ -1,4 +1,5 @@
 import unittest
+from itertools import pairwise
 
 import numpy as np
 
@@ -131,7 +132,7 @@ class SimulationTests(unittest.TestCase):
 
         self.assertGreaterEqual(len(frontier), 2)
         self.assertLessEqual(len(frontier), 15)
-        for previous, current in zip(frontier, frontier[1:], strict=True):
+        for previous, current in pairwise(frontier):
             self.assertLessEqual(previous.volatility, current.volatility + 1e-12)
             self.assertLess(previous.expected_return, current.expected_return)
 
