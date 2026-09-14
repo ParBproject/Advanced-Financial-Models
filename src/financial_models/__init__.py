691955935b98a38a3ed3487528ce28fba11fa09a
Par Bahrae <githubproproject@proton.me>
2026-09-14 00:12:26 -0400

    Export advanced portfolio analytics

--- src/financial_models/__init__.py
+++ src/financial_models/__init__.py
@@ -1,5 +1,17 @@
 """Reproducible financial-modeling core for the portfolio workbook."""
 
+from .advanced_portfolio import (
+    PortfolioPoint,
+    PortfolioSimulation,
+    approximate_efficient_frontier,
+    correlated_portfolio_metrics,
+    covariance_from_correlation,
+    illustrative_correlation_matrix,
+    max_sharpe_portfolio,
+    min_volatility_portfolio,
+    simulate_long_only_portfolios,
+    validate_covariance_matrix,
+)
 from .cash_flow import CashFlowAssumptions, CashFlowForecast, CashFlowPeriod, forecast_cash_flow
 from .credit_risk import (
     Loan,
@@ -24,9 +36,19 @@ __all__ = [
     "LoanRiskResult",
     "PortfolioCreditSummary",
     "PortfolioMetrics",
+    "PortfolioPoint",
+    "PortfolioSimulation",
+    "approximate_efficient_frontier",
     "assess_loan",
+    "correlated_portfolio_metrics",
+    "covariance_from_correlation",
     "forecast_cash_flow",
+    "illustrative_correlation_matrix",
+    "max_sharpe_portfolio",
+    "min_volatility_portfolio",
     "portfolio_metrics",
+    "simulate_long_only_portfolios",
     "summarize_portfolio",
+    "validate_covariance_matrix",
     "workbook_balanced_portfolio",
 ]
